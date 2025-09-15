8a826b6526edbf2c5678f3c2ccf879dfce37156b
SAMPATH <sampukummikr@gmail.com>
2025-09-15 22:55:54 +0530

    Recursive call to print 9 tables

--- src/Java_Basics/Lab_01.java
+++ src/Java_Basics/Lab_01.java
@@ -1,7 +1,31 @@
 package Java_Basics;
 
-public class Lab_01 {
-    public static void main(String[] args) {
-        System.out.println ("Heloo adiyen let us run");
+public class Lab_01
+{
+    public static void main(String[] args)
+    {
+        //int a = 10;
+//        System.out.println ("Heloo adiyen let us run");
+//        System.out.print ("Heloo adiyen let us run");
+//        System.out.print ("2Heloo adiyen let us run");
+//        System.out.printf ("Heloo adiyen let us run and a is %d",a);
+        // Table of 9 using printf command only
+        int c = 9;
+        int i = 1;
+        ninetables(c,i);
+
     }
+    public static void ninetables(int c,int i)
+   {
+            System.out.printf("printing nine tables %d*%d=%d\n", c, i, (c * i));
+           if (i<10)
+           {
+               i++;
+               ninetables (c,i);
+           }
+          // else return;
+   }
+
+
+
 }
